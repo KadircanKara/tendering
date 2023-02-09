2c73b597d2a4a0ceac0437122a42aa082a651ca4
Kadircan Kara <kadircann.kara@gmail.com>
2023-02-09 15:02:36 +0300

    Changed local path

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ from app_pages import *
 from git import Repo
 import os
 
-LOCAL_PATH = os.path.dirname(os.getcwd())
+LOCAL_PATH = os.path.abspath(__file__)
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.VAPOR], suppress_callback_exceptions=True, meta_tags=[{'name': 'viewport',
                              'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
