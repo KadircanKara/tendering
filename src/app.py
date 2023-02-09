60d73820a2aeb13d847c55a6dfa43aa8d545d4fa
Kadircan Kara <kadircann.kara@gmail.com>
2023-02-09 13:42:26 +0300

    Removed Cloning

--- src/app.py
+++ src/app.py
@@ -12,7 +12,6 @@ from git import Repo
 import os
 
 LOCAL_PATH = os.path.dirname(os.getcwd())
-Repo.clone_from('https://github.com/KadircanKara/tendering.git' , LOCAL_PATH)
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.VAPOR], suppress_callback_exceptions=True, meta_tags=[{'name': 'viewport',
                              'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
